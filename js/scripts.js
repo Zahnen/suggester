5e990d973f0c1af82c53222171b7a86f24ea2fc8
Zahnen Garner <zahnen@gmail.com>
2020-10-23 11:01:30 -0700

    add branching logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
   $("form#userInfo").submit(function(event) {
+    const name = $("input#name").val();
+    const frustration = $("select#frustration").val();
+    const experience = $("select#prevExperience").val();
+
     $("#results").show();
+    $("span#name").prepend(name);  
+    
+    if (frustration === "muchFrustration") {
+      $("#JavaScript").show();
+    }
+    else if (frustration === "noFrustration" && experience === "noExperience") {
+      $("#HTML").show();
+    }
+    else if (frustration === "noFrustration" && experience === "muchExperience") {
+      $("#Python").show();
+    }
+    else {
+      $("#CSharp").show();
+    }
     event.preventDefault();
   });
 });
